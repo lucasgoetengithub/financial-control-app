404a509a2dcb754883eec604e277512ca2624565
lucasgoetengithub <lucasgoetengithub@gmail.com>
2023-04-13 23:43:34 -0300

    teste

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '../views/App';
 import Login from '../views/Login';
 
+const Private = ({ Item }) => {
+    const user = JSON.parse(localStorage.getItem("user_token"));    
+
+    // const { signed } = useAuth();
+    // var logado = false;
+    // if (user != null) {
+    //     logado = true;
+    // } else {
+    //     logado = signed;
+    // }
+    
+    // return logado ? <Item/> : <Login/>
+    return <Item/>;
+}
 
 export default function Routers() {
     return (
@@ -11,7 +25,8 @@ export default function Routers() {
         <Fragment>
                 <Routes>
                     <Route path="/" element={<App />} />
-                    <Route path='/login' element={<Login/>}/>
+                    {/* <Route path="/login" element={<Login />} /> */}
+                    <Route exact path="/login" element={<Private Item={Login} />} />
                     {/* <Route path="/Register" element={<Register />} />
                     <Route exact path="/home" element={<Private Item={Home} />} />
                     <Route exact path="/history" element={<Private Item={HistoryCards} />} />

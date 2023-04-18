3078e0b11bb07fe615bcd35fb0fc85cdcf6435c6
lucasgoetengithub <lucasgoetengithub@gmail.com>
2023-04-18 17:15:51 -0300

    teste

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,56 +1,56 @@
-import { createContext, useEffect, useState } from "react";
-import api from '../service/api.js';
+// import { createContext, useEffect, useState } from "react";
+// import api from '../service/api.js';
 
-export const AuthContext = createContext({});
+// export const AuthContext = createContext({});
 
-export const AuthProvider = function({ children }) {
-  // const [user, setUser] = useState();
+// export const AuthProvider = function({ children }) {
+//   // const [user, setUser] = useState();
 
-  // useEffect(() => {
-  //   const userToken = localStorage.getItem("user_token");
-  //   const usersStorage = localStorage.getItem("users_bd");
+//   // useEffect(() => {
+//   //   const userToken = localStorage.getItem("user_token");
+//   //   const usersStorage = localStorage.getItem("users_bd");
 
-  //   if (userToken && usersStorage) {
-  //     const hasUser = JSON.parse(usersStorage)?.filter(
-  //       (user) => user.email === JSON.parse(userToken).email
-  //     );
+//   //   if (userToken && usersStorage) {
+//   //     const hasUser = JSON.parse(usersStorage)?.filter(
+//   //       (user) => user.email === JSON.parse(userToken).email
+//   //     );
 
-  //     if (hasUser) setUser(hasUser[0]);
-  //   }
-  // }, []);
+//   //     if (hasUser) setUser(hasUser[0]);
+//   //   }
+//   // }, []);
 
-  // const signin = (email, token) => {
-  //   localStorage.setItem("user_token", JSON.stringify({ email, token }));
-  //   setUser({ email, token });
+//   // const signin = (email, token) => {
+//   //   localStorage.setItem("user_token", JSON.stringify({ email, token }));
+//   //   setUser({ email, token });
 
-  //   return;
+//   //   return;
     
-  // };
+//   // };
 
-  // const signup = (name, email, password) => {
+//   // const signup = (name, email, password) => {
      
-  //   api.post(`/users`, {
-  //     name: name,
-  //     email: email,
-  //     password: password,
-  //     perfis: ["CLIENTE"]
-  //   }).then(() => {})
+//   //   api.post(`/users`, {
+//   //     name: name,
+//   //     email: email,
+//   //     password: password,
+//   //     perfis: ["CLIENTE"]
+//   //   }).then(() => {})
 
-  //   return;
-  // };
+//   //   return;
+//   // };
 
-  // const signout = () => {
-  //   setUser(null);
-  //   localStorage.removeItem("user_token");
-  // };
+//   // const signout = () => {
+//   //   setUser(null);
+//   //   localStorage.removeItem("user_token");
+//   // };
 
-  // return (
-  //   <AuthContext.Provider
-  //     value={{ user, signed: !!user, signin, signup, signout }}
-  //   >
-  //     {children}
-  //   </AuthContext.Provider>
-  // );
-};
+//   // return (
+//   //   <AuthContext.Provider
+//   //     value={{ user, signed: !!user, signin, signup, signout }}
+//   //   >
+//   //     {children}
+//   //   </AuthContext.Provider>
+//   // );
+// };
 
-export default AuthContext;
+// export default AuthContext;

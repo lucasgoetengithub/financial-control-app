f5974c069b387ab8ed03e11ec85da41b780f663a
Julia dos Santos <santos.ju1995@gmail.com>
2024-08-21 23:51:14 -0300

    validacaotoken

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
      //baseURL: 'https://financial-control-api.herokuapp.com'
-     baseURL: 'http://localhost:8080/'
+     baseURL: 'http://localhost:8081/'
 });
 
 export default api;
